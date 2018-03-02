b060c3965a3942b2afb91c2968d8ac63f0de452f
Patrick Tuite <patricktuite@gmail.com>
2018-03-02 18:32:41 -0500

    first commit

---
+++ content/my-first-post.md
@@ -0,0 +1,10 @@
+Title: First Post!
+Date: 2018-03-02 18:14
+Modified: 2018-03-02 18:14
+Category: misc
+Tags: Pelican, learning, python
+Slug: My-first-post
+Authors: Patrick Tuite
+Summary: Pelican blog to document learning
+
+I am starting this little blog to give me a space to document what I am currently learning about and force me to update Github everyday. Today I learned about Pelican a static site generator. I was able to successfully deploy this post by creating a virtual environment with conda and installing Pelican. I did the quick-install and several blog post and walkthroughs were extremely helpful in my understanding and execution. I also worked on some basic Javascript including intializing variables. 
---
+++ pelicanconf.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Patrick Tuite'
+SITENAME = "Patrick's Blog"
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Personal Site', 'http://www.patricktuite.com'),)
+
+# Social widget
+SOCIAL = (('Twitter', 'https://twitter.com/patricktuite'),
+          ('GitHub', 'https://github.com/patricktuite'),)
+
+DEFAULT_PAGINATION = 10
+
+STATIC_PATHS = ['images']
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
